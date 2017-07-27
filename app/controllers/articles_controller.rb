85201906cf8db09821aeff5bf4f419b726605d4b
Rails Project <alan_lee1994@hotmail.com>
2017-07-27 06:23:37 +0000

    add association between user and association,debugger view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,6 +16,8 @@ class ArticlesController < ApplicationController
     #creating the create action
     def create
         @article = Article.new(article_params)
+        @article.user = User.first
+        
         if @article.save 
             flash[:success] = "Article was successfully created!"
             redirect_to article_path(@article)
