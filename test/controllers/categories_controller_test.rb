f8844cde11bc559986834e5ea6e75fd42c66e704
Rails Project <alan_lee1994@hotmail.com>
2017-07-28 14:49:26 +0000

    fix tests and update navbar with categories

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class CategoriesControllerTest < ActionController::TestCase
     def setup
         @category = Category.create(name: "sports")
+        @user = User.create(username: "john", email: "john@example.com", password: "password", admin: true)
     end
     
    test "should get categories index" do
@@ -11,12 +12,20 @@ class CategoriesControllerTest < ActionController::TestCase
    end
    
    test "should get new" do
+        post login_path, params: {session:{email: @user.email, password: @user.password}}
         get :new
         assert_response :success
    end
    
    test " should get show" do
-       get( :show, {'id' => @category.id})
+       get(:show, {'id' => @category.id})
        assert_response :success
+   end
+   
+   test "should redirect create when admin is not logged in" do
+       assert_no_difference 'Category.count' do
+           post :create, category: {name: "sports"}
+       end
+       assert_redirected_to categories_path
    end
 end
